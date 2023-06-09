f56c8299c1b32704f5ec2d8806a5e31041a2c81c
Dimitris Kyriakis <kyriakds@gmail.com>
2023-06-09 16:11:27 -0400

    Add_manual_Violin

--- Scripts/Manual_Violin_Plots.R
+++ Scripts/Manual_Violin_Plots.R
@@ -1,5 +1,5 @@
 # Set working directory
-setwd("C:/Feng Data/CoPImmunoPD/")
+setwd("C:/Feng Data/CoPImmunoPD/draw_violin_r/")
 
 
 # Install required libraries
@@ -9,8 +9,6 @@ require(ggplot2)
 require(Seurat)
 
 
-# Path of plots
-resultdir <- "./draw_violin_r/"
 
 # Load Object
 Subbbb <- readRDS("Subset_raarranged.rds")
@@ -20,10 +18,10 @@ genes_to_plot <- c("GZMA", "GZMB")
 
 title_genes <- paste0(paste(genes_to_plot,collapse="_"))
 p <- VlnPlot(Subbbb, features=genes_to_plot,stack=T,group.by="CCellType",split.by="Condition",cols=c("black","red"),flip =T)
-
+p
 # Save Plot as pdf
 # In case you select to many genes maybe you need to adjust height or width of the saved plot
 # e.g ggsave(plot=p,filename=paste0(resultdir,"Violin_Plot",title_genes,".pdf"), width=12, height=10)
-ggsave(plot=p,filename=paste0(resultdir,"Violin_Plot",title_genes,".pdf"))
+ggsave(plot=p,filename=paste0("Violin_Plot",title_genes,".pdf"))
 # Save Plot as png
-ggsave(plot=p,filename=paste0(resultdir,"Violin_Plot",title_genes,".png"))
+ggsave(plot=p,filename=paste0("Violin_Plot",title_genes,".png"))
