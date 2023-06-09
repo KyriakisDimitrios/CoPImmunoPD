b1fd80bf93a10622af6b6f1e13b1541a4c43856e
Dimitris Kyriakis <kyriakds@gmail.com>
2023-06-09 14:49:58 -0400

    Add_manual_Violin

--- Scripts/Manual_Violin_Plots.R
+++ Scripts/Manual_Violin_Plots.R
@@ -10,6 +10,8 @@ title_genes <- paste0(paste(features,collapse="_"))
 p <- VlnPlot(Subbbb, features=genes_to_plot,stack=T,group.by="CCellType",split.by="Condition",cols=c("black","red"),flip =T)
 
 # Save Plot as pdf
+# In case you select to many genes maybe you need to adjust height or width of the saved plot
+# e.g ggsave(plot=p,filename=paste0(resultdir,"Violin_Plot",title_genes,".pdf"), width=12, height=10)
 ggsave(plot=p,filename=paste0(resultdir,"Violin_Plot",title_genes,".pdf"))
 # Save Plot as png
 ggsave(plot=p,filename=paste0(resultdir,"Violin_Plot",title_genes,".png"))
