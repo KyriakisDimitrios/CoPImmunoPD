310afb624190330e4cadcd3a0d75fd2255203be2
Dimitris Kyriakis <kyriakds@gmail.com>
2023-06-12 14:28:33 -0400

    Density COunts and positve cells

--- Scripts/Functions.R
+++ Scripts/Functions.R
@@ -547,11 +547,9 @@ Combined_Percent_Expressing <- function(
     col_dim_names <- names(percent_expressing)
     mat_dims <- list(row_dim_names, col_dim_names)
     final_df <- data.frame(matrix(unlist(percent_expressing), nrow = length(features_list), byrow = FALSE, dimnames = mat_dims), stringsAsFactors = FALSE)
+    expression_info <- expression_info[colnames(seurat_object),]
 
-
-
-
-    return(final_df)
+    return(list(final_df=final_df,expression_info=expression_info))
 }
 
 
@@ -579,4 +577,295 @@ Is_Seurat <- function(
   } else {
     return(rhs)
   }
+}
+
+
+
+Overall_dens_Count_Joint_plot <- function(Seurat_CCR7pos,Seurat_CCR7neg,features,resultdir){
+    title_genes_1 <- paste0(paste(features,collapse="+"),"+")
+    title_genes_2 <- paste(features,collapse="_")
+    
+    # ================================== Positive CCR7 =================================
+    data_df_res_all <- Combined_Percent_Expressing(
+        seurat_object=Seurat_CCR7pos,
+        features= features,
+        threshold = 0,
+        group_by = "CellType",
+        split_by = "Condition",
+        entire_object = FALSE,
+        slot = "counts",
+        assay = "RNA"
+    )
+    data_df_res <- data_df_res_all$final_df
+    expression_info <- data_df_res_all$expression_info[colnames(Seurat_CCR7pos),]
+    expression_info_vec <- as.vector(expression_info[[title_genes_1]])
+    names(expression_info_vec) <- rownames(x = expression_info)
+    Seurat_CCR7pos <- AddMetaData(
+        object = Seurat_CCR7pos,
+        metadata = expression_info_vec,
+        col.name = title_genes_2
+    )
+
+    # ================================== NEGATTIVE CCR7 =================================
+    data_df_res_all <- Combined_Percent_Expressing(
+        seurat_object=Seurat_CCR7neg,
+        features= features,
+        threshold = 0,
+        group_by = "CellType",
+        split_by = "Condition",
+        entire_object = FALSE,
+        slot = "counts",
+        assay = "RNA"
+    )
+    data_df_res <- data_df_res_all$final_df
+    expression_info <- data_df_res_all$expression_info[colnames(Seurat_CCR7neg),]
+    expression_info_vec <- as.vector(expression_info[[title_genes_1]])
+    names(expression_info_vec) <- rownames(x = expression_info)
+    Seurat_CCR7neg <- AddMetaData(
+        object = Seurat_CCR7neg,
+        metadata = expression_info_vec,
+        col.name = title_genes_2
+    )
+
+
+
+    UP_VECTOR <-   FetchData(object = Seurat_CCR7pos, vars = title_genes_2)
+    down_VECTOR <-   FetchData(object = Seurat_CCR7neg, vars = title_genes_2)
+    if(sum(UP_VECTOR)==0){
+        p2.1 <- FeaturePlot(Seurat_CCR7pos,  features=title_genes_2,pt.size=0.2,order=T,split.by = "Combined", cols = c("gray","gray")) +patchwork::plot_layout(ncol = 4, nrow = 1)
+    }else{
+        p2.1 <- FeaturePlot(Seurat_CCR7pos,  features=title_genes_2,pt.size=0.2,order=T,split.by = "Combined") +patchwork::plot_layout(ncol = 4, nrow = 1)
+    }
+    if(sum(down_VECTOR)==0){
+        p2.2 <- FeaturePlot(Seurat_CCR7neg,  features=title_genes_2,pt.size=0.2,order=T,split.by = "Combined", cols = c("gray","gray")) +patchwork::plot_layout(ncol = 4, nrow = 1)
+    }else{
+        p2.2 <- FeaturePlot(Seurat_CCR7neg,  features=title_genes_2,pt.size=0.2,order=T,split.by = "Combined") +patchwork::plot_layout(ncol = 4, nrow = 1)
+    }
+
+    p2 <- p2.1/p2.2 
+    # &   scale_colour_gradientn(colours = rev(brewer.pal(n = 11, name = "Blues")),limits = range(c(0, 1)))
+    #)scale_colour_continuous(limits = range(c(0, 1)))
+    ggsave(plot=p2,filename=paste0(resultdir,"Split_Counts_",title_genes_2,".pdf"),width=12,height=8)
+    return(p2)
+}
+
+
+
+
+
+
+
+myDotPlot <- function(
+  object,
+  assay = NULL,
+  features,
+  cols = c("grey90", "#C51B7D"),
+  col.min = -2.5,
+  col.max = 2.5,
+  dot.min = 0,
+  dot.scale = 6,
+  idents = NULL,
+  group.by = NULL,
+  split.by = NULL,
+  cluster.idents = FALSE,
+  scale = TRUE,
+  scale.by = 'radius',
+  scale.min = NA,
+  scale.max = NA
+) {
+  assay <- assay %||% DefaultAssay(object = object)
+  DefaultAssay(object = object) <- assay
+  split.colors <- !is.null(x = split.by) && !any(cols %in% rownames(x = brewer.pal.info))
+  scale.func <- switch(
+    EXPR = scale.by,
+    'size' = scale_size,
+    'radius' = scale_radius,
+    stop("'scale.by' must be either 'size' or 'radius'")
+  )
+  feature.groups <- NULL
+  if (is.list(features) | any(!is.na(names(features)))) {
+    feature.groups <- unlist(x = sapply(
+      X = 1:length(features),
+      FUN = function(x) {
+        return(rep(x = names(x = features)[x], each = length(features[[x]])))
+      }
+    ))
+    if (any(is.na(x = feature.groups))) {
+      warning(
+        "Some feature groups are unnamed.",
+        call. = FALSE,
+        immediate. = TRUE
+      )
+    }
+    features <- unlist(x = features)
+    names(x = feature.groups) <- features
+  }
+  cells <- unlist(x = CellsByIdentities(object = object, idents = idents))
+
+  data.features <- FetchData(object = object, vars = features, cells = cells)
+  data.features$id <- if (is.null(x = group.by)) {
+    Idents(object = object)[cells, drop = TRUE]
+  } else {
+    object[[group.by, drop = TRUE]][cells, drop = TRUE]
+  }
+  if (!is.factor(x = data.features$id)) {
+    data.features$id <- factor(x = data.features$id)
+  }
+  id.levels <- levels(x = data.features$id)
+  data.features$id <- as.vector(x = data.features$id)
+  if (!is.null(x = split.by)) {
+    splits <- object[[split.by, drop = TRUE]][cells, drop = TRUE]
+    if (split.colors) {
+      if (length(x = unique(x = splits)) > length(x = cols)) {
+        stop("Not enough colors for the number of groups")
+      }
+      cols <- cols[1:length(x = unique(x = splits))]
+      names(x = cols) <- unique(x = splits)
+    }
+    data.features$id <- paste(data.features$id, splits, sep = '_')
+    unique.splits <- unique(x = splits)
+    id.levels <- paste0(rep(x = id.levels, each = length(x = unique.splits)), "_", rep(x = unique(x = splits), times = length(x = id.levels)))
+  }
+  data.plot <- lapply(
+    X = unique(x = data.features$id),
+    FUN = function(ident) {
+      data.use <- data.features[data.features$id == ident, 1:(ncol(x = data.features) - 1), drop = FALSE]
+      avg.exp <- apply(
+        X = data.use,
+        MARGIN = 2,
+        FUN = function(x) {
+          return(mean(x = expm1(x = x)))
+        }
+      )
+      pct.exp <- apply(X = data.use, MARGIN = 2, FUN = PercentAbove, threshold = 0)
+      return(list(avg.exp = avg.exp, pct.exp = pct.exp))
+    }
+  )
+  names(x = data.plot) <- unique(x = data.features$id)
+  if (cluster.idents) {
+    mat <- do.call(
+      what = rbind,
+      args = lapply(X = data.plot, FUN = unlist)
+    )
+    mat <- scale(x = mat)
+    id.levels <- id.levels[hclust(d = dist(x = mat))$order]
+  }
+  data.plot <- lapply(
+    X = names(x = data.plot),
+    FUN = function(x) {
+      data.use <- as.data.frame(x = data.plot[[x]])
+      data.use$features.plot <- rownames(x = data.use)
+      data.use$id <- x
+      return(data.use)
+    }
+  )
+  data.plot <- do.call(what = 'rbind', args = data.plot)
+  if (!is.null(x = id.levels)) {
+    data.plot$id <- factor(x = data.plot$id, levels = id.levels)
+  }
+  ngroup <- length(x = levels(x = data.plot$id))
+  if (ngroup == 1) {
+    scale <- FALSE
+    warning(
+      "Only one identity present, the expression values will be not scaled",
+      call. = FALSE,
+      immediate. = TRUE
+    )
+  } else if (ngroup < 5 & scale) {
+    warning(
+      "Scaling data with a low number of groups may produce misleading results",
+      call. = FALSE,
+      immediate. = TRUE
+    )
+  }
+  avg.exp.scaled <- sapply(
+    X = unique(x = data.plot$features.plot),
+    FUN = function(x) {
+      data.use <- data.plot[data.plot$features.plot == x, 'pct.exp']
+      if (scale) {
+        data.use <- scale(x = data.use)
+        data.use <- MinMax(data = data.use, min = col.min, max = col.max)
+      } else {
+        data.use <- log1p(x = data.use)
+      }
+      return(data.use)
+    }
+  )
+  avg.exp.scaled <- as.vector(x = t(x = avg.exp.scaled))
+  if (split.colors) {
+    avg.exp.scaled <- as.numeric(x = cut(x = avg.exp.scaled, breaks = 20))
+  }
+  data.plot$avg.exp.scaled <- avg.exp.scaled
+  data.plot$features.plot <- factor(
+    x = data.plot$features.plot,
+    levels = features
+  )
+  data.plot$pct.exp[data.plot$pct.exp < dot.min] <- NA
+  data.plot$pct.exp <- data.plot$pct.exp * 100
+  if (split.colors) {
+    splits.use <- vapply(
+      X = as.character(x = data.plot$id),
+      FUN = gsub,
+      FUN.VALUE = character(length = 1L),
+      pattern =  paste0(
+        '^((',
+        paste(sort(x = levels(x = object), decreasing = TRUE), collapse = '|'),
+        ')_)'
+      ),
+      replacement = '',
+      USE.NAMES = FALSE
+    )
+    data.plot$colors <- mapply(
+      FUN = function(color, value) {
+        return(colorRampPalette(colors = c('grey', color))(20)[value])
+      },
+      color = cols[splits.use],
+      value = avg.exp.scaled
+    )
+  }
+  color.by <- ifelse(test = split.colors, yes = 'colors', no = 'pct.exp')
+  if (!is.na(x = scale.min)) {
+    data.plot[data.plot$pct.exp < scale.min, 'pct.exp'] <- scale.min
+  }
+  if (!is.na(x = scale.max)) {
+    data.plot[data.plot$pct.exp > scale.max, 'pct.exp'] <- scale.max
+  }
+  if (!is.null(x = feature.groups)) {
+    data.plot$feature.groups <- factor(
+      x = feature.groups[data.plot$features.plot],
+      levels = unique(x = feature.groups)
+    )
+  }
+  plot <- ggplot(data = data.plot, mapping = aes_string(x = 'features.plot', y = 'id')) +
+    geom_point(mapping = aes_string(size = 'pct.exp', color = color.by)) +
+    scale.func(range = c(0, dot.scale), limits = c(scale.min, scale.max)) +
+    theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
+    guides(size = guide_legend(title = 'Percent Expressed')) +
+    labs(
+      x = 'Features',
+      y = ifelse(test = is.null(x = split.by), yes = 'Identity', no = 'Split Identity')
+    ) +
+    theme_cowplot()
+  if (!is.null(x = feature.groups)) {
+    plot <- plot + facet_grid(
+      facets = ~feature.groups,
+      scales = "free_x",
+      space = "free_x",
+      switch = "y"
+    ) + theme(
+      panel.spacing = unit(x = 1, units = "lines"),
+      strip.background = element_blank()
+    )
+  }
+  if (split.colors) {
+    plot <- plot + scale_color_identity()
+  } else if (length(x = cols) == 1) {
+    plot <- plot + scale_color_distiller(palette = cols)
+  } else {
+    plot <- plot + scale_color_gradient(low = cols[1], high = cols[2])
+  }
+  if (!split.colors) {
+    plot <- plot + guides(color = guide_colorbar(title = 'Percent Expressed'))
+  }
+  return(plot)
 }
